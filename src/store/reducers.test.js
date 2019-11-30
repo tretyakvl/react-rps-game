90808b6a377807e85b650eb3d0fad61370a62ccc
Sane <tretyakvl@gmail.com>
2019-11-30 18:02:21 +0300

    Writes PLAY_AGAIN reducer test

--- src/store/reducers.test.js
+++ src/store/reducers.test.js
@@ -1,13 +1,38 @@
 import { PLAY, PLAY_AGAIN } from './actionTypes'
 import reducer from './reducers'
+import { CHIP_NAMES } from '../constants'
+
+const { ROCK, PAPER, SCISSORS } = CHIP_NAMES
 
 describe('reducer', () => {
   it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual({
+    const expectedState = {
       score: 0,
       playerChoice: null,
       houseChoice: null,
       result: null
-    })
+    }
+
+    expect(reducer(undefined, {})).toEqual(expectedState)
+  })
+
+  it('should handle PLAY_AGAIN', () => {
+    const state = {
+      score: 2,
+      playerChoice: 'scissors',
+      houseChoice: 'scissors',
+      result: 'draw'
+    }
+    const action = {
+      type: PLAY_AGAIN
+    }
+    const expectedState = {
+      score: 2,
+      playerChoice: null,
+      houseChoice: null,
+      result: null
+    }
+
+    expect(reducer(state, action)).toEqual(expectedState)
   })
 })
