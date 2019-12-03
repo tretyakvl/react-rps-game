ddcb1b5de9d997ef0036d432c5c5d1fa014a77f0
Sane <tretyakvl@gmail.com>
2019-12-03 17:55:18 +0300

    Moves store provider to index.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import { Router } from '@reach/router'
-import { Provider } from 'react-redux'
-import store from './store'
 import Game from './components/Game/Game'
 import Rules from './components/Rules/Rules'
 
 const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Game path='/'>
-        <Rules path='rules' />
-      </Game>
-    </Router>
-  </Provider>
+  <Router>
+    <Game path='/'>
+      <Rules path='rules' />
+    </Game>
+  </Router>
 )
 
 export default App
