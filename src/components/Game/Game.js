7e0fdc288da54293ec2ad4e49dee0e44acaf682d
Sane <tretyakvl@gmail.com>
2019-12-03 20:54:08 +0300

    Connects game component to store with hooks

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Link } from '@reach/router'
+import { CHIP_NAMES } from '../../constants'
+import { play as playAction, playAgain as playAgainAction } from '../../store'
 import ScoreDisplay from '../Score/Score'
 import Chip from '../Chip/Chip'
 import './Game.css'
 
+const { ROCK, PAPER, SCISSORS } = CHIP_NAMES
+
 const Game = (props) => {
+  const score = useSelector(state => state.score)
+  const dispatch = useDispatch()
+  const play = useCallback(
+    (playerChoice) => dispatch(playAction(playerChoice)),
+    [dispatch]
+  )
+  const playAgain = useCallback(
+    (playerChoice) => dispatch(playAgainAction(playerChoice)),
+    [dispatch]
+  )
+
   return (
     <section className='Game'>
-      <ScoreDisplay score='12' />
+      <ScoreDisplay score={score} />
       <div className='Game__display'>
-        <Chip type='scissors' />
-        <Chip type='scissors' />
-        <Chip type='scissors' />
+        <Chip type={ROCK} />
+        <Chip type={PAPER} />
+        <Chip type={SCISSORS} />
       </div>
       <Link to='rules' className='Game__link'>
         Rules
