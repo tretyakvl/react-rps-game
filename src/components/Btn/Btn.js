b0dca6bcef8905c6e20e0706dd1ddb202e9b04ba
Sane <tretyakvl@gmail.com>
2019-12-13 20:34:46 +0300

    Adds different elements support to btn component

--- src/components/Btn/Btn.js
+++ src/components/Btn/Btn.js
@@ -4,22 +4,25 @@ import { Link } from 'react-router-dom'
 import './Btn.css'
 
 const Btn = (props) => {
-  const { type, to, ...rest } = props
+  const { type, to, href, ...rest } = props
+  let classes = 'Btn'
+
+  if (type) classes += ` ${type}`
 
   if (to) {
     return (
-      <Link className='Btn' to={to} {...rest}>{props.children}</Link>
+      <Link className={classes} to={to} {...rest}>{props.children}</Link>
     )
   }
 
-  if (type === 'button') {
+  if (href) {
     return (
-      <button className='Btn' {...rest}>{props.children}</button>
+      <a className={classes} href={href} {...rest}>{props.children}</a>
     )
   }
 
   return (
-    <button className='Btn' {...rest}>{props.children}</button>
+    <button className={classes} {...rest}>{props.children}</button>
   )
 }
 
