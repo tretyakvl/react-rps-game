fd196a937fe044b4037e9d1c8acc3e5568dc7c97
Sane <tretyakvl@gmail.com>
2019-12-25 16:51:03 +0300

    Updates

---
+++ precache-manifest.9a2869f53c97ce69883430d70868d343.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8765c1f98b05138246b93c6373c521fa",
+    "url": "/react-rps-game/index.html"
+  },
+  {
+    "revision": "b3c546df3f6a6dd3d9ad",
+    "url": "/react-rps-game/static/css/main.3badf215.chunk.css"
+  },
+  {
+    "revision": "533dc2f45bb1ea568031",
+    "url": "/react-rps-game/static/js/2.44691743.chunk.js"
+  },
+  {
+    "revision": "b3c546df3f6a6dd3d9ad",
+    "url": "/react-rps-game/static/js/main.6fcd66cb.chunk.js"
+  },
+  {
+    "revision": "4f9d38f8cbdb09292c3d",
+    "url": "/react-rps-game/static/js/runtime-main.73c8cfef.js"
+  },
+  {
+    "revision": "07cdd3ea259cb042967eaa53cc10296a",
+    "url": "/react-rps-game/static/media/bg-pentagon.07cdd3ea.svg"
+  },
+  {
+    "revision": "60a30ae9893bf93234f7fc67758eaa20",
+    "url": "/react-rps-game/static/media/bg-triangle.60a30ae9.svg"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/react-rps-game/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "7d2e0b18d2860b90407f41297dce461f",
+    "url": "/react-rps-game/static/media/icon-lizard.7d2e0b18.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/react-rps-game/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/react-rps-game/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/react-rps-game/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "b6446e8e1e5d5609deb209d401e779a3",
+    "url": "/react-rps-game/static/media/icon-spock.b6446e8e.svg"
+  },
+  {
+    "revision": "f19a40fbc5296effc230454fa1295890",
+    "url": "/react-rps-game/static/media/image-rules-bonus.f19a40fb.svg"
+  },
+  {
+    "revision": "5d74d25d9c64bde346585ec7e7587a69",
+    "url": "/react-rps-game/static/media/image-rules.5d74d25d.svg"
+  }
+]);
